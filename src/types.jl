6a85de972b7586f3d0f698af4705f4c5d0cfd168
gabrielgellner <gabrielgellner@gmail.com>
2015-10-19 21:29:17 -0700

    Added some thoughts on future directions

--- src/types.jl
+++ src/types.jl
@@ -8,12 +8,13 @@ order(b::Tableau) = b.order
 # eltype with signature:
 Base.convert{Tnew <: Real}(::Type{Tnew}, tab::Tableau) = error("Define convert method for concrete Tableau types")
 
+## Solver Output Types
 abstract AbstractODESolution
 
 #TODO: add solver information. Look at what is going on in `Optim.jl`
 type RKODESolution <: AbstractODESolution
     # I currently hard code the types as this is the most common case. I can
-    # look into generalizing this, but I will be wary of makign the code overly
+    # look into generalizing this, but I will be wary of making the code overly
     # complex to do so
     x::Array{Float64, 1}
     y::Array{Float64, 2}
